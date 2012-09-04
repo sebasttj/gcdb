e5477c147c8c2d457d6a5ff6cbd578202e051812
Josh Sebastian <sebasttj+git@gmail.com>
2012-09-04 17:18:09 -0500

    Checkin working versions of the schema and ingest script, and some documentation.

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
 gcdb
 ====
 
-A database for storing NFL game data, with support for importing from nfl.com's GameCenter
+   A database for storing NFL game data, with support for importing from
+   nfl.com's GameCenter
+
+Authors
+-------
+   Josh Sebastian
+
+
+About
+-----
+
+   Gcdb is a database which stores play-by-play information about NFL games. It
+   records home and away team names, points scored per quarter, and play-by-play
+   data. There is also a hierarchical categorization of play types.
+   
+   The idea is that you can use this data to answer questions that cannot
+   generally be answered by on-line tools. See the "Examples" section for some
+   ideas.
+   
+   Included is a script that can parse a nfl.com GameCenter JSON file, and add
+   its data to gcdb.
+
+Installation
+------------
+
+   Gcdb requires a PostgreSQL database, and the ingest script requires
+   Python 2 with the psycopg2, json, logging, and argparse modules. On many
+   systems, the psycoph2 module is not included simply by installing Python, it
+   must be specifically requested as a separate package.
+   
+   On a Debian-based system, you can install all reqiuired components like so:
+   
+    # apt-get install postgresql python-psycopg2
+   
+   Once Postgres is installed, the simplest way to proceed is to set up a user
+   account for yourself using auth ident, and give it with full control to a
+   blank database called "gcdb". On a Debian-based system, that would look like
+   this (run as yourself):
+   
+    $ sudo -u postgres createuser $USER
+    $ sudo -u postgres createdb -O $USER gcdb
+   
+   Now that permissions are set up and we have a blank database, we can import
+   the gcdb schema:
+   
+    $ psql -f gcdb-schema.sql gcdb
+   
+   Now, all that's left to do is import some data. Download a game from
+   nfl.com, and ingest it.
+   
+    $ wget http://www.nfl.com/liveupdate/game-center/2012020500/2012020500_gtd.json
+    $ ./gcdb-ingest-game 2012020500_gtd.json
+   
+   Voila!
+
+Examples
+--------
+
+   So now you've got gcdb, and you have data in your database. What now? Well,
+   how about we want to know whether run or pass was more successful on
+   4th-and-3. Issue the following query:
+   
+    select actionid, action_yards - yards_to_go > 0, count(*)
+    from drive_action
+    where down = 4 and yards_to_go = 3
+      and actionid in (10, 11, 14, 15, 16, 19, 20)
+    group by actionid, action_yards - yards_to_go > 0
+   
+   (Those actionids represent, respectively, rush, rush for TD, incompletion,
+   completion, completion for TD, interception, and sack. When the stat
+   hierarchy is done, that query will be a lot more friendly.)
---
+++ gcdb-schema.sql
@@ -0,0 +1,422 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.1.5
+-- Dumped by pg_dump version 9.1.5
+-- Started on 2012-09-04 17:05:51 CDT
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- TOC entry 173 (class 3079 OID 11677)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 1966 (class 0 OID 0)
+-- Dependencies: 173
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 161 (class 1259 OID 16499)
+-- Dependencies: 5
+-- Name: drive; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE drive (
+    gameid character(10) NOT NULL,
+    driveid integer NOT NULL,
+    pos_team character(3) NOT NULL
+);
+
+
+ALTER TABLE public.drive OWNER TO gcdb;
+
+--
+-- TOC entry 167 (class 1259 OID 16528)
+-- Dependencies: 5
+-- Name: game; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE game (
+    gameid character varying(50) NOT NULL,
+    home_team character(3) NOT NULL,
+    away_team character(3) NOT NULL,
+    season integer,
+    week integer
+);
+
+
+ALTER TABLE public.game OWNER TO gcdb;
+
+--
+-- TOC entry 162 (class 1259 OID 16502)
+-- Dependencies: 5
+-- Name: play; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE play (
+    gameid character(10) NOT NULL,
+    driveid integer NOT NULL,
+    playid integer NOT NULL,
+    down integer,
+    yards_to_go integer,
+    "time" integer NOT NULL,
+    yardline integer,
+    description character(2000)
+);
+
+
+ALTER TABLE public.play OWNER TO gcdb;
+
+--
+-- TOC entry 163 (class 1259 OID 16508)
+-- Dependencies: 5
+-- Name: play_action; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE play_action (
+    gameid character(10) NOT NULL,
+    playid integer NOT NULL,
+    playerid character(10),
+    sequence integer NOT NULL,
+    actionid integer NOT NULL,
+    yards integer
+);
+
+
+ALTER TABLE public.play_action OWNER TO gcdb;
+
+--
+-- TOC entry 164 (class 1259 OID 16511)
+-- Dependencies: 5
+-- Name: player; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE player (
+    playerid character(10) NOT NULL,
+    name character varying(100) NOT NULL
+);
+
+
+ALTER TABLE public.player OWNER TO gcdb;
+
+--
+-- TOC entry 172 (class 1259 OID 16922)
+-- Dependencies: 1930 5
+-- Name: drive_action; Type: VIEW; Schema: public; Owner: gcdb
+--
+
+CREATE VIEW drive_action AS
+    SELECT gameid, game.season, game.week, game.home_team, game.away_team, play.driveid, play.playid, play."time", drive.pos_team, play.down, play.yards_to_go, play.description AS play_description, player.name AS player_name, play_action.actionid, play_action.yards AS action_yards, play_action.sequence FROM ((((game JOIN drive USING (gameid)) JOIN play USING (gameid, driveid)) JOIN play_action USING (gameid, playid)) JOIN player USING (playerid));
+
+
+ALTER TABLE public.drive_action OWNER TO gcdb;
+
+--
+-- TOC entry 169 (class 1259 OID 16685)
+-- Dependencies: 5
+-- Name: quarter_score; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE quarter_score (
+    gameid character(10) NOT NULL,
+    quarter integer NOT NULL,
+    team character(3) NOT NULL,
+    points integer NOT NULL
+);
+
+
+ALTER TABLE public.quarter_score OWNER TO gcdb;
+
+--
+-- TOC entry 170 (class 1259 OID 16695)
+-- Dependencies: 1928 5
+-- Name: game_score; Type: VIEW; Schema: public; Owner: gcdb
+--
+
+CREATE VIEW game_score AS
+    SELECT x.gameid, x.home_team, x.away_team, x.home_points, x.away_points, CASE WHEN (x.home_points > x.away_points) THEN x.home_team WHEN (x.home_points < x.away_points) THEN x.away_team ELSE 'tie'::bpchar END AS winning_team FROM (SELECT gameid, game.home_team, game.away_team, sum(h.points) AS home_points, sum(a.points) AS away_points FROM ((game JOIN quarter_score h USING (gameid)) JOIN quarter_score a USING (gameid, quarter)) WHERE ((h.team = game.home_team) AND (a.team = game.away_team)) GROUP BY gameid, game.home_team, game.away_team) x;
+
+
+ALTER TABLE public.game_score OWNER TO gcdb;
+
+--
+-- TOC entry 165 (class 1259 OID 16514)
+-- Dependencies: 5
+-- Name: stat; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE stat (
+    statid character varying(200) NOT NULL,
+    description character varying(1000),
+    has_yards boolean NOT NULL
+);
+
+
+ALTER TABLE public.stat OWNER TO gcdb;
+
+--
+-- TOC entry 166 (class 1259 OID 16520)
+-- Dependencies: 5
+-- Name: stat_action_leaf; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE stat_action_leaf (
+    statid character(100) NOT NULL,
+    actionid integer NOT NULL
+);
+
+
+ALTER TABLE public.stat_action_leaf OWNER TO gcdb;
+
+--
+-- TOC entry 168 (class 1259 OID 16531)
+-- Dependencies: 5
+-- Name: stat_hierarchy; Type: TABLE; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE TABLE stat_hierarchy (
+    parent character(100) NOT NULL,
+    child character(100) NOT NULL
+);
+
+
+ALTER TABLE public.stat_hierarchy OWNER TO gcdb;
+
+--
+-- TOC entry 171 (class 1259 OID 16700)
+-- Dependencies: 1929 5
+-- Name: stat_action; Type: VIEW; Schema: public; Owner: gcdb
+--
+
+CREATE VIEW stat_action AS
+    WITH RECURSIVE hier AS (SELECT stat_action_leaf.statid, stat_action_leaf.actionid FROM stat_action_leaf UNION ALL SELECT n.parent, p.actionid FROM (stat_hierarchy n JOIN hier p ON ((n.child = p.statid)))) SELECT hier.statid, hier.actionid FROM hier;
+
+
+ALTER TABLE public.stat_action OWNER TO gcdb;
+
+--
+-- TOC entry 1932 (class 2606 OID 16535)
+-- Dependencies: 161 161 161 1960
+-- Name: PK_DRIVE; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY drive
+    ADD CONSTRAINT "PK_DRIVE" PRIMARY KEY (gameid, driveid);
+
+
+--
+-- TOC entry 1946 (class 2606 OID 16537)
+-- Dependencies: 167 167 1960
+-- Name: PK_GAME; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY game
+    ADD CONSTRAINT "PK_GAME" PRIMARY KEY (gameid);
+
+
+--
+-- TOC entry 1935 (class 2606 OID 16539)
+-- Dependencies: 162 162 162 1960
+-- Name: PK_PLAY; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY play
+    ADD CONSTRAINT "PK_PLAY" PRIMARY KEY (gameid, playid);
+
+
+--
+-- TOC entry 1940 (class 2606 OID 16541)
+-- Dependencies: 164 164 1960
+-- Name: PK_PLAYER; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY player
+    ADD CONSTRAINT "PK_PLAYER" PRIMARY KEY (playerid);
+
+
+--
+-- TOC entry 1938 (class 2606 OID 16543)
+-- Dependencies: 163 163 163 163 1960
+-- Name: PK_PLAY_ACTION; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY play_action
+    ADD CONSTRAINT "PK_PLAY_ACTION" PRIMARY KEY (gameid, playid, sequence);
+
+
+--
+-- TOC entry 1950 (class 2606 OID 16689)
+-- Dependencies: 169 169 169 169 1960
+-- Name: PK_QUARTER_SCORE; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY quarter_score
+    ADD CONSTRAINT "PK_QUARTER_SCORE" PRIMARY KEY (gameid, quarter, team);
+
+
+--
+-- TOC entry 1942 (class 2606 OID 16545)
+-- Dependencies: 165 165 1960
+-- Name: PK_STAT; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY stat
+    ADD CONSTRAINT "PK_STAT" PRIMARY KEY (statid);
+
+
+--
+-- TOC entry 1944 (class 2606 OID 16547)
+-- Dependencies: 166 166 166 1960
+-- Name: PK_STAT_ACTION; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY stat_action_leaf
+    ADD CONSTRAINT "PK_STAT_ACTION" PRIMARY KEY (statid, actionid);
+
+
+--
+-- TOC entry 1948 (class 2606 OID 16549)
+-- Dependencies: 168 168 168 1960
+-- Name: PK_STAT_HIER; Type: CONSTRAINT; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+ALTER TABLE ONLY stat_hierarchy
+    ADD CONSTRAINT "PK_STAT_HIER" PRIMARY KEY (parent, child);
+
+
+--
+-- TOC entry 1936 (class 1259 OID 16550)
+-- Dependencies: 163 1960
+-- Name: IDX_PLAY_ACTION_ACTIONID; Type: INDEX; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE INDEX "IDX_PLAY_ACTION_ACTIONID" ON play_action USING btree (actionid);
+
+
+--
+-- TOC entry 1933 (class 1259 OID 16551)
+-- Dependencies: 162 1960
+-- Name: IDX_PLAY_DOWN; Type: INDEX; Schema: public; Owner: gcdb; Tablespace: 
+--
+
+CREATE INDEX "IDX_PLAY_DOWN" ON play USING btree (down);
+
+
+--
+-- TOC entry 1951 (class 2606 OID 16552)
+-- Dependencies: 167 1945 161 1960
+-- Name: FK_DRIVE_GAME; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY drive
+    ADD CONSTRAINT "FK_DRIVE_GAME" FOREIGN KEY (gameid) REFERENCES game(gameid);
+
+
+--
+-- TOC entry 1953 (class 2606 OID 16557)
+-- Dependencies: 162 163 163 1934 162 1960
+-- Name: FK_PLAY_ACTION_GAMEID_PLAYID; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY play_action
+    ADD CONSTRAINT "FK_PLAY_ACTION_GAMEID_PLAYID" FOREIGN KEY (gameid, playid) REFERENCES play(gameid, playid);
+
+
+--
+-- TOC entry 1954 (class 2606 OID 16562)
+-- Dependencies: 163 1939 164 1960
+-- Name: FK_PLAY_ACTION_PLAYERID; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY play_action
+    ADD CONSTRAINT "FK_PLAY_ACTION_PLAYERID" FOREIGN KEY (playerid) REFERENCES player(playerid);
+
+
+--
+-- TOC entry 1952 (class 2606 OID 16567)
+-- Dependencies: 162 162 161 161 1931 1960
+-- Name: FK_PLAY_GAME; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY play
+    ADD CONSTRAINT "FK_PLAY_GAME" FOREIGN KEY (gameid, driveid) REFERENCES drive(gameid, driveid);
+
+
+--
+-- TOC entry 1958 (class 2606 OID 16690)
+-- Dependencies: 169 1945 167 1960
+-- Name: FK_QUARTER_SCORE_GAMEID; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY quarter_score
+    ADD CONSTRAINT "FK_QUARTER_SCORE_GAMEID" FOREIGN KEY (gameid) REFERENCES game(gameid);
+
+
+--
+-- TOC entry 1955 (class 2606 OID 16572)
+-- Dependencies: 166 165 1941 1960
+-- Name: FK_STAT_ACTION_STATID; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY stat_action_leaf
+    ADD CONSTRAINT "FK_STAT_ACTION_STATID" FOREIGN KEY (statid) REFERENCES stat(statid);
+
+
+--
+-- TOC entry 1956 (class 2606 OID 16577)
+-- Dependencies: 1941 168 165 1960
+-- Name: FK_STAT_HIER_CHILD; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY stat_hierarchy
+    ADD CONSTRAINT "FK_STAT_HIER_CHILD" FOREIGN KEY (child) REFERENCES stat(statid);
+
+
+--
+-- TOC entry 1957 (class 2606 OID 16582)
+-- Dependencies: 1941 168 165 1960
+-- Name: FK_STAT_HIER_PARENT; Type: FK CONSTRAINT; Schema: public; Owner: gcdb
+--
+
+ALTER TABLE ONLY stat_hierarchy
+    ADD CONSTRAINT "FK_STAT_HIER_PARENT" FOREIGN KEY (parent) REFERENCES stat(statid);
+
+
+--
+-- TOC entry 1965 (class 0 OID 0)
+-- Dependencies: 5
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2012-09-04 17:05:52 CDT
+
+--
+-- PostgreSQL database dump complete
+--
+
